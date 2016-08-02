0d0eb2932d1cb63517634886393fdd72e4a24581
kuier <838113965@qq.com>
2016-08-02 17:53:37 +0800

    进制转换

---
+++ 华为机试/5-进制转换/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * 题目描述
+
+写出一个程序，接受一个十六进制的数值字符串，输出该数值的十进制字符串。（多组同时输入 ）
+
+输入描述:
+输入一个十六进制的数值字符串。
+
+
+输出描述:
+输出该数值的十进制字符串。
+
+输入例子:
+0xA
+
+输出例子:
+10
+ */
+namespace _5_进制转换
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
